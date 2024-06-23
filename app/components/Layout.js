fd3a6b2928e7d0343a53384e7affea5f4ba4cb38
Leanne Goldsmith <leannegoldsmith@Leannes-MacBook-Pro.local>
2024-06-23 13:27:01 +0100

    style navbar

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -14,9 +14,14 @@ export default function Layout({ children }) {
     <div>
       <Navbar />
       <main className="p-4">
-        <section className="flex flex-col items-center justify-center min-h-[60vh]">
-          <h1 className="text-4xl font-bold">Welcome to Our Recipe Collection</h1>
-          <p className="mt-4 text-gray-200">Discover a variety of cuisines and find your favorite recipes.</p>
+        <section className="flex flex-col items-center justify-center min-h-[80vh]">
+          <h1 className="text-7xl md:text-9xl font-bold text-center">Make it Delicious</h1>
+          <p className="mt-4 text-gray-200 px-10 text-center">
+          Global Grub Hub is your top destination for exploring popular cuisines from
+          around the world in one spot. Discover diverse international dishes, from
+          Italian to Mexican, Indian to Chinese, and more. Find new recipes, culinary
+          inspiration, and experience global flavors all in one place at Global Grub Hub.
+          </p>
           <SearchBar onSearch={handleSearch} />  {/* Add the SearchBar component */}
         </section>
         <CuisineGrid />
--- app/components/SearchBar.js
+++ app/components/SearchBar.js
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
 
 const SearchBar = ({ onSearch }) => {
   const [searchQuery, setSearchQuery] = useState('');
@@ -17,20 +18,20 @@ const SearchBar = ({ onSearch }) => {
   };
 
   return (
-    <form className="mt-4 w-full max-w-md" onSubmit={handleSearchSubmit}>
-      <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
+    <form className="mt-10 w-4/5 mx-auto" onSubmit={handleSearchSubmit}>
+      <div className="flex items-center bg-white rounded-full py-2 px-4 shadow-lg">
         <input
           type="text"
-          className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+          className="appearance-none bg-transparent border-none w-full text-gray-700 py-1 px-2 leading-tight focus:outline-none"
           placeholder="Search for recipes..."
           value={searchQuery}
           onChange={handleSearchChange}
         />
         <button
           type="submit"
-          className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+          className="flex-shrink-0 text-gray-700 hover:text-teal-500 focus:outline-none"
         >
-          Search
+          <FaSearch size={20} />
         </button>
       </div>
     </form>
